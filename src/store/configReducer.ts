a434c0b5bbf4b0678da7bda26b3b50f62e3a1bb0
Aleks <anterebol@gmail.com>
2023-11-01 23:17:19 +0300

    feat: add redux/toolkit and default settings for store

---
+++ src/store/configReducer.ts
@@ -0,0 +1,15 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import appReducer from './appReducer/appReducer';
+
+const rootReducer = combineReducers({
+  appReducer,
+});
+
+export const store = () =>
+  configureStore({
+    reducer: rootReducer,
+  });
+
+export type RootStore = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof store>;
+export type AppDispatch = AppStore['dispatch'];
