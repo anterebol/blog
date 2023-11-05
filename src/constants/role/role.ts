93f1492ba429b478953d0843febb24e804d42ec7
Aleks <anterebol@gmail.com>
2023-11-05 11:58:10 +0300

    feat: add roles constant

---
+++ src/constants/role/role.ts
@@ -0,0 +1,7 @@
+export const ROLES = {
+  ROLE_GUEST: 'guest',
+  ROLE_ADMIN: 'admin',
+  ROLE_USER: 'user',
+};
+export const ROLE = 'role';
+export const { ROLE_ADMIN, ROLE_GUEST, ROLE_USER } = ROLES;
