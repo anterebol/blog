16a0412ef138149aa0bbfec943860189b0fc6f90
Aleks <anterebol@gmail.com>
2023-11-05 12:00:25 +0300

    feat: add new Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Main } from './pages/Main/Main';
 import { Error } from './pages/Error/Error';
-import PATHES from './constants/pathes';
-
-const { MAIN, NOT_FOUND } = PATHES;
+import {
+  NOT_FOUND,
+  LOGIN,
+  MAIN,
+  SLASH,
+} from './constants/pathes/common-pathes';
+import { ADMIN, EDITOR, PREVIEW } from './constants/pathes/admin-pathes';
+import {
+  PORTFOLIO,
+  TRAVEL_BLOG,
+  LIVE_TOPICS,
+} from './constants/pathes/user-pathes';
+import { createDynamicPath } from './utils/createDynamicPath';
+import { Admin } from './pages/Admin/Admin';
 
 function App() {
   return (
     <Routes>
+      <Route path={SLASH} element={<Navigate to={MAIN} />} />
       <Route path={MAIN} element={<Main />} />
+      <Route path={LOGIN} element={LOGIN} />
+      <Route path={ADMIN} element={<Admin />} />
+      <Route path={EDITOR} element={EDITOR} />
+      <Route path={PREVIEW} element={PREVIEW} />
+      <Route path={PORTFOLIO} element={PORTFOLIO} />
+      <Route path={LIVE_TOPICS} element={LIVE_TOPICS} />
+      <Route path={createDynamicPath(LIVE_TOPICS)} element={LIVE_TOPICS} />
+      <Route path={TRAVEL_BLOG} element={TRAVEL_BLOG} />
+      <Route path={createDynamicPath(TRAVEL_BLOG)} element={TRAVEL_BLOG} />
       <Route path={NOT_FOUND} element={<Error />} />
     </Routes>
   );
