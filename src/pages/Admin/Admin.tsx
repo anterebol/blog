3045525108468c7b782199ae439328f29438cf06
Aleks <anterebol@gmail.com>
2023-11-13 13:02:59 +0300

    feat: add Admin page

---
+++ src/pages/Admin/Admin.tsx
@@ -0,0 +1,21 @@
+import './admin.css';
+import { useParams } from 'react-router-dom';
+import { SideBar } from '../../components/SideBar/SideBar';
+import { Editor } from '../../components/Editor/Editor';
+import { ArticleList } from '../../components/ArticleList/ArticleList';
+import { AsidePopupEditor } from '../../components/AsidePopupEditor/AsidePopupEditor';
+
+export function Admin() {
+  const { page, id } = useParams();
+  console.log(page, id);
+  return (
+    <div className="admin-container">
+      <SideBar />
+      {/* <AsidePopupEditor /> */}
+      <div className="aside-invisible-box" />
+      <div className="admin-container__main">
+        {page && id ? <Editor /> : <ArticleList />}
+      </div>
+    </div>
+  );
+}
