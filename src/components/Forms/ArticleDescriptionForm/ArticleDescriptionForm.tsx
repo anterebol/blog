bee40f9f1ea848e5ef7d6ffe70dcfa1fb1894c15
Aleks <anterebol@gmail.com>
2023-11-15 14:17:23 +0300

    feat: update editor form

--- src/components/Forms/ArticleDescriptionForm/ArticleDescriptionForm.tsx
+++ src/components/Forms/ArticleDescriptionForm/ArticleDescriptionForm.tsx
@@ -1,9 +1,16 @@
 import { useState } from 'react';
 import './ArticleDescriptionForm.css';
+import { months } from '../../../hoc/Private/months/months';
+
+const initialDate = {
+  year: 'Year',
+  month: 'Month',
+  day: 'Day',
+};
 
 export const ArticleDescriptionForm = () => {
   const [img, setImg] = useState('');
-  const [date, setDate] = useState('mm.day.year');
+  const [date, setDate] = useState({ ...initialDate });
 
   return (
     <form action="#/" className="description-form">
@@ -32,12 +39,22 @@ export const ArticleDescriptionForm = () => {
       ) : null}
       <span className={['datepicker-toggle'].join(' ')}>
         <div className="datepicker-toggle-button">
-          <div className="mounth">Mounth</div>
-          <div className="day">Day</div>
-          <div className="year">Year</div>
+          <div className="mounth">{date.month}</div>
+          <div className="day">{date.day}</div>
+          <div className="year">{date.year}</div>
         </div>
         <input
           type="date"
+          onChange={(e) => {
+            const currentDate = { ...date };
+            const [year, numberMonth, day] = e.target.value.split(
+              '-'
+            ) as Array<string>;
+            currentDate.year = year;
+            currentDate.month = months[numberMonth];
+            currentDate.day = day;
+            setDate({ ...currentDate });
+          }}
           required
           className={['datepicker-input'].join(' ')}
         />
