2503a786fd1c4f242d9afb075084f26f32273f35
Aleks <anterebol@gmail.com>
2023-11-23 17:27:01 +0300

    feat: update form for main article info

--- src/components/Forms/ArticleDescriptionForm/ArticleDescriptionForm.tsx
+++ src/components/Forms/ArticleDescriptionForm/ArticleDescriptionForm.tsx
@@ -1,64 +1,76 @@
-import { useState } from 'react';
 import './ArticleDescriptionForm.css';
-import { months } from '../../../hoc/Private/months/months';
-
-const initialDate = {
-  year: 'Year',
-  month: 'Month',
-  day: 'Day',
-};
+import { months } from '../../../constants/months/months';
+import { useAppDispatch, useAppSelector } from '../../../hooks/hooks';
+import {
+  setArticleDate,
+  setArticleTitle,
+  setArticleImg,
+} from '../../../store/appReducer/appReducer';
+import { ToggleButton } from '../../Buttons/ToggleButton/ToggleButton';
 
 export const ArticleDescriptionForm = () => {
-  const [img, setImg] = useState('');
-  const [date, setDate] = useState({ ...initialDate });
+  const dispatch = useAppDispatch();
+  const { articleMainInfo } = useAppSelector((state) => state.appReducer);
+  const { date, title, mainImg } = articleMainInfo;
 
   return (
     <form action="#/" className="description-form">
-      <input
-        type="text"
-        placeholder="Title for article"
-        className="description-form__title"
-      />
-      <input
-        type="text"
-        value={img}
-        onChange={(e) => setImg(e.target.value)}
-        placeholder="Main image link"
-        className={['description-form__img', img ? 'hide' : ''].join(' ')}
-      />
-      {img ? (
-        <button
-          type="button"
-          className="article-main-img"
-          onClick={() => {
-            setImg('');
-          }}
-        >
-          <img src={img} alt="main-img" />
-        </button>
-      ) : null}
-      <span className={['datepicker-toggle'].join(' ')}>
-        <div className="datepicker-toggle-button">
-          <div className="mounth">{date.month}</div>
-          <div className="day">{date.day}</div>
-          <div className="year">{date.year}</div>
-        </div>
+      <fieldset>
+        <legend>Main article information</legend>
         <input
-          type="date"
+          type="text"
+          value={title}
           onChange={(e) => {
-            const currentDate = { ...date };
-            const [year, numberMonth, day] = e.target.value.split(
-              '-'
-            ) as Array<string>;
-            currentDate.year = year;
-            currentDate.month = months[numberMonth];
-            currentDate.day = day;
-            setDate({ ...currentDate });
+            dispatch(setArticleTitle(e.target.value));
           }}
-          required
-          className={['datepicker-input'].join(' ')}
+          placeholder="Title for article"
+          className="description-form__title"
         />
-      </span>
+        <input
+          type="text"
+          value={mainImg}
+          onChange={(e) => {
+            dispatch(setArticleImg(e.target.value));
+          }}
+          placeholder="Main image link"
+          className={['description-form__img', mainImg ? 'hide' : ''].join(' ')}
+        />
+        {mainImg ? (
+          <div className="article-main-img">
+            <ToggleButton
+              func={() => {
+                dispatch(setArticleImg(''));
+              }}
+              cls={['toggle__button', 'opened']}
+            />
+            <img src={mainImg} alt="main-img" />
+          </div>
+        ) : null}
+        <div className="description-form__date">
+          <span className={['datepicker-toggle'].join(' ')}>
+            <div className="datepicker-toggle-button">
+              <div className="mounth">{date.month}</div>
+              <div className="day">{date.day}</div>
+              <div className="year">{date.year}</div>
+            </div>
+            <input
+              type="date"
+              onChange={(e) => {
+                const currentDate = { ...date };
+                const [year, numberMonth, day] = e.target.value.split(
+                  '-'
+                ) as Array<string>;
+                currentDate.year = year;
+                currentDate.month = months[numberMonth];
+                currentDate.day = day;
+                dispatch(setArticleDate(currentDate));
+              }}
+              required
+              className={['datepicker-input'].join(' ')}
+            />
+          </span>
+        </div>
+      </fieldset>
     </form>
   );
 };
