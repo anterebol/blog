ece5b1d15407eb71e3287db18739cb20c2985407
Aleks <anterebol@gmail.com>
2023-11-26 23:24:38 +0300

    refactor: update SideBar component code

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -4,16 +4,16 @@ import { ADMIN, ADMIN_ASIDE_BAR } from '../../constants/pathes/admin-pathes';
 import Link from '../NavLink/NavLink';
 import { SideBarIcon } from './SideBarIcon/SideBarIcon';
 import { sideBarImages } from '../../constants/sideBar/sideBarImages';
-import { AsideToggleButton } from '../Buttons/AsideToggleButton/AsideToggleButton';
+import { ToggleButton } from '../Buttons/ToggleButton/ToggleButton';
 import { joinClasses } from '../../utils/joinClasses';
 
 export const SideBar = () => {
   const [openBar, setOpenBar] = useState(false);
   return (
     <aside className={joinClasses(['side-bar', 'left', openBar ? 'open' : ''])}>
-      <AsideToggleButton
+      <ToggleButton
         func={() => setOpenBar(!openBar)}
-        cls={['side-bar__button', openBar ? 'opened' : '']}
+        cls={['toggle__button', openBar ? 'opened' : '']}
       />
       <ul className={joinClasses(['side-bar__list', openBar ? 'open' : ''])}>
         {Object.values(ADMIN_ASIDE_BAR).map((link) => (
--- src/components/SideBar/SideBarIcon/SideBarIcon.tsx
+++ src/components/SideBar/SideBarIcon/SideBarIcon.tsx
@@ -1,9 +1,18 @@
+import { joinClasses } from '../../../utils/joinClasses';
 import { toUpperFirstLetter } from '../../../utils/toUpperFirstLetter';
 import './sideBarIcon.css';
 
-export const SideBarIcon = ({ icon, alt }: { icon: string; alt: string }) => {
+export const SideBarIcon = ({
+  icon,
+  alt,
+  cls = [],
+}: {
+  icon: string;
+  alt: string;
+  cls?: Array<string>;
+}) => {
   return (
-    <div className="side-bar__icon">
+    <div className={joinClasses(['side-bar__icon', ...cls])}>
       <img src={icon} alt={alt} title={toUpperFirstLetter(alt)} />
     </div>
   );
