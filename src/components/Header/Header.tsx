af10b0b874ca6110b9360e15d0e5f611cc0b7715
Aleks <anterebol@gmail.com>
2023-12-06 15:51:52 +0300

    feat: update Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,24 +2,48 @@ import './header.css';
 import { useLocation } from 'react-router-dom';
 import { HeaderNav } from './HeaderNav/HeaderNav';
 import { setHeaderNav } from '../../utils/setHeaderLinks';
+import logo from '../../assets/tent.png';
+import { MAIN } from '../../constants/pathes/navPathes';
 import { ADMIN } from '../../constants/pathes/admin-pathes';
+import { ToggleButton } from '../Buttons/ToggleButton/ToggleButton';
+import { useAppDispatch } from '../../hooks/hooks';
+import { toggleAsideNav } from '../../store/appReducer/appReducer';
+import { SideNav } from '../SideNav/SideNav';
+import { joinClasses } from '../../utils/joinClasses';
 
-export const Header = ({ role }: { role: string }) => {
+const additionalHeaderClass = (page: string) => {
+  switch (true) {
+    case page.includes(ADMIN):
+      return 'header__admin';
+    case page.includes(MAIN):
+      return 'header__main';
+    default:
+      return '';
+  }
+};
+
+export const Header = () => {
+  const dispatch = useAppDispatch();
   const { pathname } = useLocation();
+
   return (
     <header
-      className={[
-        'header',
-        pathname.includes(ADMIN) ? 'header__admin' : '',
-      ].join(' ')}
+      className={joinClasses(['header', additionalHeaderClass(pathname)])}
     >
-      <div className="header__cover" />
       <div className="header__container">
         <div className="header__logo logo">
-          <p>logo</p>
+          <img src={logo} alt="logo" />
+          <h1 className="header__logo-text">Festern travel</h1>
         </div>
-        <HeaderNav links={setHeaderNav(role)} />
+        <HeaderNav links={setHeaderNav()} />
+        <ToggleButton
+          func={() => {
+            dispatch(toggleAsideNav());
+          }}
+          cls={['toggle__button', 'nav-btn']}
+        />
       </div>
+      <SideNav />
     </header>
   );
 };
--- src/components/Header/HeaderNav/HeaderNav.tsx
+++ src/components/Header/HeaderNav/HeaderNav.tsx
@@ -4,10 +4,12 @@ import './headerNav.css';
 export const HeaderNav = ({ links }: { links: Array<string> }) => {
   return (
     <nav className="header__nav">
-      <ul className="nav__list">
+      <ul className="nav">
         {links.map((link: string) => (
-          <li key={link}>
-            <Link link={link} cls={['header__link']} />
+          <li className="nav__item" key={link}>
+            <Link link={link} cls={['header__link']}>
+              <div className="nav__item-line" />
+            </Link>
           </li>
         ))}
       </ul>
