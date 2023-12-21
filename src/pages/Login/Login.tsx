9a1676459cb9f6cb4a6389afb4d29648fd95cef4
Aleks <anterebol@gmail.com>
2023-12-21 15:18:25 +0300

    feat: add button "Main page" for returning ro MAin page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,35 +1,68 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Button } from '../../components/Buttons/Button';
 import { joinClasses } from '../../utils/joinClasses';
 import './login.css';
+import { useAppDispatch } from '../../hooks/hooks';
+import { signIn } from '../../store/apiReducer/api/api';
+import { MAIN } from '../../constants/pathes/navPathes';
 
 export const Login = () => {
+  const dispatch = useAppDispatch();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
   return (
-    <div className="login-page">
-      <h2>Login</h2>
-      <form action="#/" className="login-page__form">
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          className={joinClasses(['login-page__input'])}
-          type="text"
-          placeholder="Email"
-        />
-        <p className="error-notice">Incorrect login</p>
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          className={joinClasses(['login-page__input'])}
-          type="text"
-          placeholder="Password"
-        />
-        <p className="error-notice">Incorrect password</p>
-        <Button
-          func={() => {}}
-          cls={['submit-button']}
-          title="Submit"
-          text="login"
-        />
-      </form>
-    </div>
+    <>
+      <Button
+        cls={['main-button']}
+        text="Main page"
+        title="Back main"
+        func={() => {
+          navigate(`/${MAIN}`);
+        }}
+      />
+      <div className="login-page">
+        <h2>Login</h2>
+        <form action="#/" className="login-page__form">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            value={email}
+            className={joinClasses(['login-page__input'])}
+            type="text"
+            onInput={(e: any) => {
+              setEmail(e.target.value);
+            }}
+            placeholder="Email"
+          />
+          <p className="error-notice">Incorrect login</p>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            value={password}
+            className={joinClasses(['login-page__input'])}
+            type="text"
+            onInput={(e: any) => {
+              setPassword(e.target.value);
+            }}
+            placeholder="Password"
+          />
+          <p className="error-notice">Incorrect password</p>
+          <Button
+            func={() => {
+              dispatch(signIn({ email, password }));
+              setEmail('');
+              setPassword('');
+              navigate(`/${MAIN}`);
+            }}
+            cls={['submit-button']}
+            title="Submit"
+            text="login"
+          />
+        </form>
+      </div>
+    </>
   );
 };
