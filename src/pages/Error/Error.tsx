d7cb5448928f1b88cc5ce0333b36043ae96d4187
Aleks <anterebol@gmail.com>
2024-01-16 13:22:46 +0300

    feat: add update Error page

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,3 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../../components/Buttons/Button';
+import attention from '../../assets/error.png';
+import './error.css';
+
 export function Error() {
-  return <div>Error</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="error-page">
+      <div className="error-page__body">
+        <div className="error-page__img">
+          <img src={attention} alt="Attention" />
+        </div>
+        <p className="error-page__information">Something went wrong...</p>
+        <Button
+          cls={['error-page__button']}
+          func={() => {
+            navigate(-1);
+          }}
+          text="Go Back"
+          title="back"
+        />
+      </div>
+    </div>
+  );
 }
